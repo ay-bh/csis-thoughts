7446cd5f0a0b84304209e7d3cfc18453a99a4431
Ayush Bhardwaj <96904351+ay-bh@users.noreply.github.com>
2023-12-21 11:30:15 +0530

    fixed creator name leak

--- app/api/post/route.js
+++ app/api/post/route.js
@@ -4,13 +4,18 @@ import { unstable_noStore as noStore } from 'next/cache';
 
 export const GET = async (request) => {
     try {
-        await connectToDB()
+        await connectToDB();
         noStore();
-        const posts = await Post.find({}).populate('creator')
-        
-
-        return new Response(JSON.stringify(posts), { status: 200 })
+        const posts = await Post.find({}).populate('creator');
+        const modifiedPosts = posts.map((post) => {
+            if (post.anon) {
+                post.creator = null; 
+            }
+            return post;
+        });
+        return new Response(JSON.stringify(modifiedPosts), { status: 200 });
     } catch (error) {
-        return new Response("Failed to fetch all posts", { status: 500 })
+        console.log(error)
+        return new Response("Failed to fetch all posts", { status: 500 });
     }
-} 
+};
